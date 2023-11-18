f1932faf266cadb503c1188fc514b5626ca04222
TinTinWinata <86914748+TinTinWinata@users.noreply.github.com>
2023-11-18 12:11:13 +0700

    trigger: build

--- src/utils/date-helper.tsx
+++ src/utils/date-helper.tsx
@@ -3,6 +3,7 @@ export function getSecondsBetweenDates(date1: Date, date2: Date): number {
   const timeDifferenceInMilliseconds = Math.abs(
     date1.getTime() - date2.getTime()
   );
+
   // Convert the time difference to seconds
   const timeDifferenceInSeconds = timeDifferenceInMilliseconds / 1000;
   return timeDifferenceInSeconds;
